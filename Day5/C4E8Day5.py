32559074f6915e64cf038791a4ce59b7c0ef2cdb
Tien <phamtien1709@gmail.com>
2017-02-27 09:20:06 +0700

    Add Check_lose

--- Day5/C4E8Day5.py
+++ Day5/C4E8Day5.py
@@ -7,6 +7,10 @@ cy = 5
 
 screen_width = 7
 screen_height = 7
+def check_lose(bx,by,screen_height,screen_width):
+    if [bx,by] == [0,0] or [bx,by] == [0,screen_height-1] or [bx,by] == [screen_width-1,0] or [bx,by] == [screen_width-1,screen_height-1]:
+        return True
+    return False
 
 def show_map(screen_height,screen_width,px,py,bx,by,cx,cy):
     for y in range(screen_height):
@@ -56,13 +60,27 @@ while win:
 
     if check_box(bx,by,next_px,next_py):
         [next_bx,next_by] = move(bx,by,dx,dy)
-        if not in_map(next_bx, next_by, screen_width, screen_height):
-            print("Can't move!!!")
+        if check_lose(next_bx, next_by, screen_height, screen_width):
+            show_map(screen_height,screen_width,next_px,next_py,next_bx,next_by,cx,cy)
+            restart = input("YOU LOSE!! DO YOU WANT TO RESTART??(Y/N)\t")
+            if restart.upper() == "Y":
+                px = 2
+                py = 1
+                bx = 3
+                by = 3
+                cx = 5
+                cy = 5
+            else:
+                print("See you again!")
+                win = False
         else:
-            bx = next_bx
-            by = next_by
-            px = next_px
-            py = next_py
+            if not in_map(next_bx, next_by, screen_width, screen_height):
+                print("Can't move!!!")
+            else:
+                bx = next_bx
+                by = next_by
+                px = next_px
+                py = next_py
     else:
         if not in_map(next_px, next_py, screen_width, screen_height):
             print("Go away!!!")
@@ -70,7 +88,7 @@ while win:
             px = next_px
             py = next_py
 
-    if check_win(cx,cy,bx,by):
+    if check_win(cx,cy,bx,by) and win == True:
         show_map(screen_height, screen_width, px, py, bx, by, cx, cy)
         print("CONGRATULATION!! YOU WIN!")
         win = False
