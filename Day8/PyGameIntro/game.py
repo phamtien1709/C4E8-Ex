a633577dab2016007cd1e650d3d57d117fabf018
Tien <phamtien1709@gmail.com>
2017-03-07 20:56:18 +0700

    Sokoban one box one point

--- Day8/PyGameIntro/game.py
+++ Day8/PyGameIntro/game.py
@@ -4,6 +4,7 @@ pygame.init()
 screen = pygame.display.set_mode([800, 600])
 
 done = False
+game_finished = False
 
 # draw background
 COLOR_WHITE = (255, 255, 255)
@@ -12,13 +13,33 @@ COLOR_BLACK = (0, 0, 0)
 # draw image
 player_image = pygame.image.load("mario.png")
 square_image = pygame.image.load("square.png")
+box_image = pygame.image.load("box.png")
+key_image = pygame.image.load("key.png")
+win_image = pygame.image.load("win.png")
 
 x = 100
 y = 100
 
 SQUARE_SIZE = 32
 
+class Key:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def calc_next_position(self, dx, dy):
+        return self.x + dx, self.y + dy
+
+class Box:
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -35,18 +56,29 @@ class Map:
         self.width = width
         self.height = height
         self.player = Player(1, 1)
+        self.box = Box(6, 6)
+        self.key = Key (11, 13)
 
     def move_player(self, dx, dy):
         [next_player_x, next_player_y] = self.player.calc_next_position(dx, dy)
+        [next_box_x, next_box_y] = self.box.calc_next_position(dx, dy)
         if self.check_inside(next_player_x, next_player_y):
-            self.player.move(dx, dy)
-
+            if next_player_x == self.box.x and next_player_y == self.box.y:
+                if self.check_inside(next_box_x, next_box_y):
+                    self.box.move(dx, dy)
+                    self.player.move(dx, dy)
+            else:
+                self.player.move(dx, dy)
 
     def check_inside(self, x, y):
         return 0 <= x < self.width and 0 <= y < self.height
 
-map = Map(25, 18)
+    def check_win(self):
+        if self.box.x == self.key.x and self.box.y == self.key.y:
+            return True
+        return False
 
+map = Map(25, 18)
 
 
 while not done:
@@ -72,16 +104,26 @@ while not done:
     # Process game events
     if dx != 0 or dy != 0:
         map.move_player(dx, dy)
+        if map.check_win():
+            print("YOU WIN!")
+            game_finished = True
 
 
     # Repaint
     screen.fill(COLOR_WHITE)
 
-    for y in range(map.width):
-        for x in range(map.height):
+    for x in range(map.width):
+        for y in range(map.height):
             screen.blit(square_image, (x * SQUARE_SIZE, y * SQUARE_SIZE))
 
+    screen.blit(key_image, (map.key.x * SQUARE_SIZE, map.key.y * SQUARE_SIZE))
+
+    screen.blit(box_image, (map.box.x * SQUARE_SIZE, map.box.y * SQUARE_SIZE))
 
     screen.blit(player_image, (map.player.x * SQUARE_SIZE, map.player.y * SQUARE_SIZE))
+
+    if game_finished == True:
+        screen.blit(win_image, (250, 250))
+        # done = True
 
     pygame.display.flip()
