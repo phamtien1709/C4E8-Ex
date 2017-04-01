e49753e9ccca5d538f469b2ecb567017b75883d4
Tien <phamtien1709@gmail.com>
2017-04-01 12:06:37 +0700

    no message

--- FlaskIntro/flaskintro.py
+++ FlaskIntro/flaskintro.py
@@ -1,17 +1,38 @@
 from flask import *
 import mlab
 from models.FoodItem import FoodItem
+from models.user import User
+import os
+from werkzeug.utils import *
+from flask_login import *
+from sessionuser import SessionUser
 
 app = Flask(__name__)
 
 # connect to mlab
 mlab.connect()
-# creat new FoodItem and save it to database
-# new_food = FoodItem()
-# new_food.src = "http://liztraks.com/ltphotography/wp-content/uploads/2016/01/2-1-450x675.jpg"
-# new_food.title = "Item 1"
-# new_food.description = "Description for Item 1"
-# new_food.save()
+
+app.config['UPLOAD_PATH'] = os.path.join(app.root_path, 'uploads')
+if not os.path.exists(app.config['UPLOAD_PATH']):
+    os.makedirs(app.config['UPLOAD_PATH'])
+
+app.secret_key = 'phamtien'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# admin_user = User()
+# admin_user.username = 'admin'
+# admin_user.password = 'admin'
+# admin_user.save()
+
+
+@login_manager.user_loader
+def user_loader(user_token):
+    found_user = User.objects(token=user_token).first()
+    if found_user:
+        session_user = SessionUser(found_user.id)
+        return session_user
 
 girl_list = [
     {
@@ -100,9 +121,31 @@ food_list2 = [
 
 @app.route('/')
 def hello_world():
-    return redirect(url_for("foodblog"))
+    return redirect(url_for("foodblog1"))
 
 number_of_visiter = 0
+
+
+@app.route('/login1', methods=["GET", "POST"])
+def login1():
+    if request.method == "GET":
+        return render_template("login1.html")
+    elif request.method == "POST":
+        user = User.objects(username=request.form['username']).first()
+        if user and user.password == request.form['password']:
+            session_user = SessionUser(user.id)
+            user.update(set__token=str(user.id))
+            login_user(session_user)
+
+            return redirect(url_for('add_food'))
+        else:
+            return render_template("login1.html")
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login1'))
 
 
 @app.route('/login')
@@ -143,16 +186,33 @@ def foodblog1():
 
 
 @app.route('/addFood', methods=["GET", "POST"])
+@login_required
 def add_food():
     if request.method == "GET":
         return render_template("addfood.html")
     elif request.method == "POST":
-        new_food = FoodItem()
-        new_food.src = request.form["source"]
-        new_food.title = request.form["title"]
-        new_food.description = request.form["description"]
-        new_food.save()
-        return render_template("addfood.html")
+        file = request.files['source']
+        if file:
+            filename = secure_filename(file.filename)
+            if os.path.exists(os.path.join(app.config['UPLOAD_PATH'], filename)):
+                name_index = 0
+                original_name = filename.rsplit('.', 1)[0]
+                original_extension = filename.rsplit('.', 1)[1]
+                while os.path.exists(os.path.join(app.config['UPLOAD_PATH'], filename)):
+                    name_index += 1
+                    filename = "{0} ({1}).{2}".format(original_name, name_index, original_extension)
+            file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+            new_food = FoodItem()
+            new_food.src = url_for('uploaded_file', filename=filename)
+            new_food.title = request.form["title"]
+            new_food.description = request.form["description"]
+            new_food.save()
+            return render_template("addfood.html")
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
 
 
 @app.route('/deleteFood', methods=["GET", "POST"])
